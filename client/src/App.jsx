52a71f3a46036e3b936ee93922798c97a16e6260
misraozdemir <s358966@studenti.polito.it>
2026-06-17 13:21:17 +0200

    added protected route to game and ranking page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import InstructionPage from './pages/InstructionsPage.jsx';
 import LoginPage from './pages/LoginPage';
 import GamePage from './pages/GamePage';
 import RankingPage from './pages/RankingPage';
-import {UserProvider} from "./contexts/UserContext.jsx";
+import {UserProvider, useUser} from "./contexts/UserContext.jsx";
 import AppNavbar from "./components/Navbar.jsx";
 
+function ProtectedRoute({ children }) {
+    const { user } = useUser();
+    // If not logged in, redirect to login page
+    if (!user) return <Navigate to="/login" />;
+    // If logged in, render the page normally
+    return children;
+}
 function App() {
     return (
         <UserProvider>
@@ -14,8 +21,16 @@ function App() {
                 <Routes>
                     <Route path="/" element={<InstructionPage/>}/>
                     <Route path="/login" element={<LoginPage/>}/>
-                    <Route path="/game" element={<GamePage/>}/>
-                    <Route path="/ranking" element={<RankingPage/>}/>
+                    <Route path="/game" element={
+                        <ProtectedRoute>
+                             <GamePage/>
+                        </ProtectedRoute>
+                    }/>
+                    <Route path="/ranking" element={
+                        <ProtectedRoute>
+                            <RankingPage/>
+                        </ProtectedRoute>
+                    }/>
                 </Routes>
             </BrowserRouter>
         </UserProvider>
